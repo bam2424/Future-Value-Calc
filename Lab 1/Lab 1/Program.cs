b84b44d28207a39324a8db45e21457b06ff26e69
brandon <141857607+bam2424@users.noreply.github.com>
2023-11-17 13:34:05 -0500

    Add files via upload

---
+++ Lab 1/Lab 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Lab 1
+// CIS 199-02-4222
+// Due: 1/23/2022
+// By: R2215
+// This program displays student's Grading ID,
+// hobbies, favorite book and movie to the console.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID:  R2215");
+            Console.WriteLine("Hobbies: Video games and watching Netflix");
+            Console.WriteLine("Favorite Book:   The Hobbit");
+            Console.WriteLine("Favorite movie:  The Irishman");
+        }
+    }
+}
